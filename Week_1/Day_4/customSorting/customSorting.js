132453d5fb443e420d5b95984aa4f8ad0e3a601d
Maggie <tong.zhu@mail.utoronto.ca>
2018-09-27 18:30:12 +0000

    add customSorting file

---
+++ Week_1/Day_4/customSorting/customSorting.js
@@ -0,0 +1,18 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+function customSort(obj1,obj2,entireAry){
+  //var compareName = compare(obj1.name,obj2.name);
+
+  if(obj1.name === obj2.name){
+    return obj1.age - obj2.age;
+  }else {
+    return obj1.name.localeCompare(obj2.name); //how else i can compare strings
+  }
+}
+
+console.log(students.sort(customSort));
