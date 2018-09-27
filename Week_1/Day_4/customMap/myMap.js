147a000a3b572a16d07d11a3933c8a7e09ec08c9
Maggie <tong.zhu@mail.utoronto.ca>
2018-09-27 19:22:25 +0000

    add myMap

---
+++ Week_1/Day_4/customMap/myMap.js
@@ -0,0 +1,23 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
+
+function map(inputAry, func){
+  var result = [];
+  for(var str of inputAry){
+
+    result.push (func(str));
+  }
+  console.log(result);
+}
