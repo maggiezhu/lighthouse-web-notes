6e7329f98fcdd98f28c5f5c101363ba285e59d07
Maggie <tong.zhu@mail.utoronto.ca>
2018-10-01 17:45:02 +0000

    add step5

---
+++ Week_2/Step4/getHTML.js
@@ -0,0 +1,27 @@
+var https = require('https');
+var buffer = '';
+function getHTML(options,callback){
+  https.get(options, function(response){
+    response.on('data',function(data){
+      buffer+= data;
+    });
+    response.on('end',function(){
+      callback(buffer);
+    });
+
+
+  });
+
+}
+
+function printHTML(buffer){
+  console.log(buffer);
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions,printHTML);
