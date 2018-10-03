d95cbeb78f6a6e2d6f83213fe0f75b0e484599b1
Maggie <tong.zhu@mail.utoronto.ca>
2018-10-03 03:12:33 +0000

    added EJS practice

--- Week_2/Weekend/fs/readFile.js
+++ Week_2/Weekend/fs/readFile.js
@@ -5,5 +5,7 @@ fs.readFile(path,function(err, data){
   if(err){
     return console.error(err);
   }
-  console.log('asynchoronous'+data);
+  console.log('asynchoronous' + data);
 });
+
+console.log('this line should print before the file data');
