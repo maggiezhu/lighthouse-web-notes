d6e837778a42ed5df030971a781c776b0a928fbd
maggiezhu <tong.zhu@mail.utoronto.ca>
2018-10-04 19:22:52 +0000

    added username login

--- Week_2/TinyApp/express_server.js
+++ Week_2/TinyApp/express_server.js
@@ -2,12 +2,15 @@ var express = require("express");
 var app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
 var PORT = 8080;
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 function generateRandomString(){
   return Math.random().toString(36).replace('0.','').slice(0,6);
@@ -16,18 +19,34 @@ function generateRandomString(){
 app.set("view engine", "ejs");
 
 app.get('/urls',(request,response)=>{
-  response.render("urls_index", {urls:urlDatabase});
+  let templateVars = {urls:urlDatabase}
+  console.log(request.headers);
+  console.log('in get /urls', request.cookies==={});
+  // if(request.cookies.username){
+  //   console.log('in get /urls, username exist');
+  //   templateVars.username =request.cookies.username}
+  //   console.log('in get /urls, username NOT exist');
+  // response.render("urls_index", templateVars);
 });
 
 app.get('/urls/new',(request,response)=>{
-  response.render('urls_new');
+  templateVars = {username:request.cookies.username}
+  response.render('urls_new',templateVars);
 });
 
+//handle POST request from /login
+app.post('/login',(request,response)=>{
+  let username = request.body.username;
+  response.cookie('username', username);
+  response.redirect('/urls');
+})
 
+//Delete shorturl
 app.post('/urls/:id/delete', (request,response)=>{
   delete urlDatabase[request.params.id];
   response.redirect('/urls');
 })
+
 
 //Edit a longurl
 app.post('/urls/:id',(request,response)=>{
@@ -45,10 +64,12 @@ app.post('/urls',(request, response)=>{
 
 
 app.get('/urls/:id',(request, response)=>{
+
   let templateVars = { shorturl: request.params.id,
-  longurl : urlDatabase[request.params.id]};
+  longurl : urlDatabase[request.params.id],
+  username:request.cookies.username};
   response.render('urls_show',templateVars); //send input url to template
-  console.log(templateVars);
+  console.log(templateVars)
 });
 
 app.get("/u/:shortURL", (request, response) => {
@@ -57,12 +78,6 @@ app.get("/u/:shortURL", (request, response) => {
   let longurl = urlDatabase[shorturl];
   response.redirect(longurl);
 });
-
-//Delete shorturl
-app.post('/urls/:id/delete', (request,response)=>{
-  delete urlDatabase[request.params.id];
-  response.redirect('/urls');
-})
 
 
 
